148cc57ad04a1f02eb8c18abf379a7b39e0e4344
lokiyolauregrace <lokiyolaure@gmail.com>
2026-01-14 10:37:08 +0100

    trein update

--- euronoom/src/Trein.java
+++ euronoom/src/Trein.java
@@ -1,12 +1,12 @@
 public class Trein {
-    public String type; // Class 373 of 374
+    public String type;
     public int aantalWagons;
     public int capaciteit;
 
     public Trein(String type, int wagons) {
         this.type = type;
 
-        // Regel: max wagons bepalen op basis van type
+
         int maxWagons;
         if (type.equals("Class 373")) {
             maxWagons = 12;
@@ -14,14 +14,14 @@ public class Trein {
             maxWagons = 14;
         }
 
-        // Als invoer te hoog is, zetten we het op het maximum
+
         if (wagons > maxWagons) {
             this.aantalWagons = maxWagons;
         } else {
             this.aantalWagons = wagons;
         }
 
-        // Capaciteit berekenen: 80 in loco + 50 per wagon
+
         this.capaciteit = 80 + (this.aantalWagons * 50);
     }
 }
